60c0cb151051cc23ab8054885fb6ad48790756d2
J <jacob.navaratne@outlook.com>
2026-02-17 16:37:33 +0000

    basic test for script file

---
+++ script.js
@@ -0,0 +1,9 @@
+// the script file for my html lets hope this works
+// Select the cookie image
+const cookie = document.querySelector("#cookie-container img");
+
+// Add a click event
+cookie.addEventListener("click", () => {
+  // Visible feedback since you can't use DevTools
+  alert("Cookie clicked!");
+});
